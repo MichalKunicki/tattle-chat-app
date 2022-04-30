caa16387c1dd75ec81f088a2261277154193be1f
Michal Kunicki <michalkunicki96@gmail.com>
2022-04-30 21:01:55 +0200

    added heroku backend link

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -27,8 +27,8 @@ const Auth = () => {
 
     const { username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "http://localhost:5000/auth";
-    // const URL = 'https://medical-pager.herokuapp.com/auth';
+    // const URL = "http://localhost:5000/auth";
+    const URL = "https://tattle-chat-app.herokuapp.com/auth";
 
     const {
       data: { token, userId, hashedPassword, fullName },
