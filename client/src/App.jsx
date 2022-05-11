bc9c1e83eb8dc0ac2daaab1fd0190f49d02d2ec9
Michal Kunicki <michalkunicki96@gmail.com>
2022-05-11 16:02:25 +0200

    small fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import "./App.css";
 
 const cookies = new Cookies();
 
-const apiKey = "rmd92m9c99ww";
+const apiKey = "exh2avch22nb";
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
